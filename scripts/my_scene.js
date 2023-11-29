0dbc391897f77b09456d82def9352252d0579e6f
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2023-11-29 14:38:30 +0900

    演習1-5

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -13,6 +13,7 @@ class MyScene extends Phaser.Scene {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
+        this.load.image('jori', 'assets/jori.png');
     }
 
     // シーン初期化処理
@@ -20,23 +21,37 @@ class MyScene extends Phaser.Scene {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');        
         this.player1 =this.add.image(500, 350, 'taro');
-        this.player1.angle=0;
-        this.player1_direction = 1;
+        this.player2 =this.add.image(200, 350, 'jori');
+        // this.player1.angle=0;
+        // this.player1_direction = 1;
     }
     
   // 毎フレーム実行される繰り返し処理
     update() {
-        if (this.player1.x >= D_WIDTH - 100) this.player1_direction = -1;
-        if (this.player1.x <= 100) this.player1_direction = 1;
+        // if (this.player1.x >= D_WIDTH - 100) this.player1_direction = -1;
+        // if (this.player1.x <= 100) this.player1_direction = 1;
     
-        if (this.player1_direction == 1) {
-            this.player1.x += 5;// 横方向へ移動を設定
-            this.player1.y += 5;// 横方向へ移動を設定
-        }else {
-            this.player1.x -= 5;// 横方向へ移動を設定
-            this.player1.y -= 5;// 横方向へ移動を設定
+        // if (this.player1_direction == 1) {
+        //     this.player1.x += 5;// 横方向へ移動を設定
+        //     this.player1.y += 5;// 横方向へ移動を設定
+        // }else {
+        //     this.player1.x -= 5;// 横方向へ移動を設定
+        //     this.player1.y -= 5;// 横方向へ移動を設定
+        // }
+        // this.player1.angle += 5;
+
+        let cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.left.isDown) {
+            //console.log("Left!");
+            this.player1.x -= 3;// 左方向に移動
+            this.player2.x += 3;// 左方向に移動
+
+        } else if (cursors.right.isDown) {
+            //console.log("Right!");
+            this.player1.x += 3;// 右方向に移動
+            this.player2.x -= 3;// 右方向に移動
+
         }
-        this.player1.angle += 5;
     }
 
 }
