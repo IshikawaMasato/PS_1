f4a01a4de561d8ae74c7500e80c79d41bb5ac047
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2023-12-04 19:44:59 +0900

    演習1-8

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -20,13 +20,24 @@ class MyScene extends Phaser.Scene {
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');        
-        this.player1 =this.add.image(500, 350, 'taro');
-        this.player2 =this.add.image(200, 350, 'jori');
-        this.player3 =this.add.image(100, 100, 'hanako');
+        // this.player1 =this.physics.add.sprite(500, 350, 'taro');
+        // this.player2 =this.add.image(200, 350, 'jori');
+        // this.player3 =this.add.image(100, 100, 'hanako');
         // this.player1.angle=0;
         // this.player1_direction = 1;
+        
+        this._leftTime=3;
+        this._timeCounter=0;
+
+        this.physics.world.setBounds(0, 0, D_WIDTH, D_HEIGHT);
+        this.player1 = this.physics.add.sprite(500, 350, 'taro');
+        this.player3 = this.physics.add.sprite(100, 100, 'hanako');
+        this.physics.add.collider(this.player1, this.player3, this.handleCollision, null, this);
+
+        //文字の追加
         this.add.text(600,400, 'MyWorld', { fontSize: '32px', fill: '#FFF' });
         this.text =this.add.text(100, 50, "", {fontSize: '32px', fill: '#ffffff'});
+        
         ///WASDキーを検知できるようにする
         this.keys = {};
         this.keys.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -34,10 +45,11 @@ class MyScene extends Phaser.Scene {
         this.keys.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keys.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
     
   // 毎フレーム実行される繰り返し処理
-    update() {
+    update(time,delta) {
         // if (this.player1.x >= D_WIDTH - 100) this.player1_direction = -1;
         // if (this.player1.x <= 100) this.player1_direction = 1;
     
@@ -50,18 +62,41 @@ class MyScene extends Phaser.Scene {
         // }
         // this.player1.angle += 5;
 
-        let cursors = this.input.keyboard.createCursorKeys();
-        if (cursors.left.isDown) {
-            //console.log("Left!");
-            this.player1.x -= 50;// 左方向に移動
-            this.player2.x += 50;// 左方向に移動
+        // let cursors = this.input.keyboard.createCursorKeys();
+        // if (cursors.left.isDown) {
+        //     //console.log("Left!");
+        //     this.player1.x -= 50;// 左方向に移動
+        //     this.player2.x += 50;// 左方向に移動
 
-        } else if (cursors.right.isDown) {
-            //console.log("Right!");
-            this.player1.x += 50;// 右方向に移動
-            this.player2.x -= 50;// 右方向に移動
+        // } else if (cursors.right.isDown) {
+        //     //console.log("Right!");
+        //     this.player1.x += 50;// 右方向に移動
+        //     this.player2.x -= 50;// 右方向に移動
+        // }
+        // this.wasd_move(this.keys)
+
+        this._timeCounter += delta;
+        if(this._timeCounter > 3000) {
+            this._timeCounter = 0;
+            this.randPosition();
         }
-        this.wasd_move(this.keys)
+
+        if (this.cursors.left.isDown) {
+            this.player1.setVelocityX(-200);
+        } else if (this.cursors.right.isDown) {
+            this.player1.setVelocityX(200);
+        } else {
+            this.player1.setVelocityX(0);
+        }
+        
+
+        if (this.cursors.up.isDown) {
+            this.player1.setVelocityY(-200);
+        } else if (this.cursors.down.isDown) {
+            this.player1.setVelocityY(200);
+        }else {
+            this.player1.setVelocityY(0);
+        }
     }
     wasd_move(keys){
         if(keys.keyS.isDown){  //Sが押されている時
@@ -70,9 +105,20 @@ class MyScene extends Phaser.Scene {
             this.text.text ="Hello!";
         }else if(keys.keyD.isDown){ //Dが押されている時
             this.text.destroy();
-        }else if(keys.keyW.isDown){ //Dが押されている時
-            this.player3.x=Phaser.Math.Between(100, 400);
-            
         }
+        // else if(keys.keyW.isDown){ //Wが押されている時
+        //     this.player3.x=Phaser.Math.Between(100, 400);
+        // }
+    }
+    randPosition() {
+        const x = Phaser.Math.Between(200, 400);
+        const y = Phaser.Math.Between(100, 200);
+        this.player3.setPosition(x, y);
+    }
+    handleCollision() {
+        // ぶつかったときの処理
+        this.text.setText("痛い！");
+        this.text.setPosition(100, 150);
+        this.player3.setVelocity(0);
     }
 }
